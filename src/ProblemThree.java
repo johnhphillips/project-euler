f9d64266b3c8a344d86006f009cc375ae4a01634
John Phillips <john.h.phillips@clegadgetworks.com>
2013-09-11 07:35:07 -0700

    Problem Three solved
    Found solution to Problem Three, still need to clean up.

--- src/ProblemThree.java
+++ src/ProblemThree.java
@@ -7,14 +7,13 @@ public class ProblemThree {
 	
 	public static void main(String[] args) {
 	
-		int testNumber = 2147483647;
-		System.out.println(isPrime(testNumber));
+		long testNumber = 600851475143L;
+		System.out.println("Factor " + testNumber);
+		primeFactors(testNumber);
+		
 	}
-	/*
-	 * Boolean function that checks if given
-	 * number is prime
-	 */
-	public static boolean isPrime(int number) {
+	public static long primeFactors(long number) {
+//		int factorOne, factorTwo;
 		/*
 		 * Prime number is one that is divisible only
 		 * by 1 and itself
@@ -23,11 +22,71 @@ public class ProblemThree {
 		 * - test whether number is a multiple of any integer between 2
 		 *   and number^(1/2)
 		 */
-		double x; int y;
+		double x; long y;
+//		System.out.println("Number = " + number);
+		// check if number is prime
+		if( isPrime(number)) { 
+//			System.out.println("1Prime Factor = " + number);
+//			System.out.println("*****");
+			return number;
+		}
 		
 		// find square root, round, convert back to int
 		x = Math.sqrt(number);
 		y = (int)Math.round(x);
+	
+		for( int i = 2; i <= y; i++) {
+			// check if current i is factor of number
+			if( number % i == 0) { 
+//				System.out.println("Number = " + number + " i = " + i);
+//				// divide and print factors
+//				factorOne = i;
+				// check that factor is prime
+				if( isPrime(i)) {
+					System.out.println("Prime Factor = " + i);
+					number = number / i;
+//					System.out.println("New Number = " + number);
+					number = primeFactors( number);
+//					System.out.println("xxxxx " + number);
+//					i = 2;
+				}
+				// factor isn't prime
+				else {
+					System.out.println("Not Prime Factor = " + i);
+					number = primeFactors( i);
+				}
+				
+//				factorTwo = number / i;
+//				System.out.println(factorOne + " " + factorTwo);
+			 }
+		}
+		// remainder after factorization
+//		System.out.println("Last Factor = " + number);
+//		System.out.println("-----");
+		return number;
+	}
+	
+	
+	
+	
+	/*
+	 * Boolean function that checks if given
+	 * number is prime
+	 */
+	public static boolean isPrime(long number) {
+		/*
+		 * Prime number is one that is divisible only
+		 * by 1 and itself
+		 * 
+		 * Test if number is prime using trial division
+		 * - test whether number is a multiple of any integer between 2
+		 *   and number^(1/2)
+		 */
+		double x; long y;
+		
+		// find square root, round, convert back to int
+		x = Math.sqrt(number);
+		y = (long)Math.round(x);
 		
 		for( int i = 2; i <= y; i++) {
 			// check if current i is divisor of number
